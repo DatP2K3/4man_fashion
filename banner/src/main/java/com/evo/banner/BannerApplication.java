0c6735679033d3beddc334175e3192144dafdc6d
Phan Dinh Dat <ytbs1vn@gmail.com>
2025-04-30 09:55:51 +0700

    convert discovery from consul to eureka

--- banner/src/main/java/com/evo/banner/BannerApplication.java
+++ banner/src/main/java/com/evo/banner/BannerApplication.java
@@ -1,11 +1,12 @@
-package com.evo.gateway;
+package com.evo.banner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayApplication {
+public class BannerApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        SpringApplication.run(BannerApplication.class, args);
     }
 }
--- elasticsearch/src/main/java/com/evotek/elasticsearch/ElasticsearchApplication.java
+++ elasticsearch/src/main/java/com/evotek/elasticsearch/ElasticsearchApplication.java
@@ -2,6 +2,7 @@ package com.evotek.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = {"com.evotek.elasticsearch", "com.evo.common"})
 public class ElasticsearchApplication {
