eded21284bfc1eeed7faf0ee2a059c06d3ced3b3
Phan Dinh Dat <ytbs1vn@gmail.com>
2025-05-15 01:27:38 +0700

    fix/set status discount when create

--- product/src/main/java/com/evo/product/domain/Product.java
+++ product/src/main/java/com/evo/product/domain/Product.java
@@ -194,10 +194,10 @@ public class Product extends Auditor {
 
                 if (endDate.isBefore(Instant.now())) {
                     discount.setStatus(DiscountStatus.EXPIRED);
-                } else if (startDate.isBefore(Instant.now()) && Instant.now().isBefore(endDate)) {
-                    discount.setStatus(DiscountStatus.ACTIVE);
                 } else if (startDate.isAfter(Instant.now())) {
                     discount.setStatus(DiscountStatus.SCHEDULED);
+                } else {
+                    discount.setStatus(DiscountStatus.ACTIVE);
                 }
             }
         }
