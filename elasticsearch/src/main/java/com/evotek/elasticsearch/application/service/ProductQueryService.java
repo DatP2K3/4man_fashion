77674e5652862a4e652b04a01aba143c7d63b6a7
Phan Dinh Dat <ytbs1vn@gmail.com>
2025-05-29 21:34:05 +0700

    auto complete product name

--- elasticsearch/src/main/java/com/evotek/elasticsearch/application/service/ProductQueryService.java
+++ elasticsearch/src/main/java/com/evotek/elasticsearch/application/service/ProductQueryService.java
@@ -3,6 +3,10 @@ package com.evotek.elasticsearch.application.service;
 import com.evotek.elasticsearch.application.dto.request.SearchProductRequest;
 import com.evotek.elasticsearch.application.dto.response.SearchProductDTO;
 
+import java.util.List;
+
 public interface ProductQueryService {
     SearchProductDTO searchProduct(SearchProductRequest request);
+
+    List<String> autocompleteProductNames(String keyword, int limit);
 }
