c258f77e080df5904258994b056416fb302b414b
Phan Dinh Dat <dat.pd@evotek.vn>
2026-03-29 09:45:11 +0700

    fix: resolve remaining lint warnings
    - File.java: Add @Builder.Default for 'deleted' field with @SuperBuilder
    - OrderItemEntityMapper: Explicitly map/ignore mismatched fields
      (entity has 'name', domain has weight/height/width/length)
    - Note: Discount CRUD in ProductController is not a real duplicate
      (same name, different HTTP methods POST vs PUT)

--- order/src/main/java/com/fourman/order/infrastructure/persistence/mapper/OrderItemEntityMapper.java
+++ order/src/main/java/com/fourman/order/infrastructure/persistence/mapper/OrderItemEntityMapper.java
@@ -1,10 +1,22 @@
 package com.fourman.order.infrastructure.persistence.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.fourman.common.mapper.EntityMapper;
 import com.fourman.order.domain.OrderItem;
 import com.fourman.order.infrastructure.persistence.entity.OrderItemEntity;
 
 @Mapper(componentModel = "spring")
-public interface OrderItemEntityMapper extends EntityMapper<OrderItem, OrderItemEntity> {}
+public interface OrderItemEntityMapper extends EntityMapper<OrderItem, OrderItemEntity> {
+    @Override
+    @Mapping(target = "name", ignore = true)
+    OrderItemEntity toEntity(OrderItem domain);
+
+    @Override
+    @Mapping(target = "weight", ignore = true)
+    @Mapping(target = "height", ignore = true)
+    @Mapping(target = "width", ignore = true)
+    @Mapping(target = "length", ignore = true)
+    OrderItem toDomainModel(OrderItemEntity entity);
+}
